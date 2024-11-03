a7ba79f798865431bc3788c63968fade5685287f
jeancarson <jeanloucarson@gmail.com>
2024-11-03 13:26:24 +0000

    like dislike

--- app/javascript/react/src/components/QuestionDetail.jsx
+++ app/javascript/react/src/components/QuestionDetail.jsx
@@ -1,5 +1,5 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 // import { useState } from 'react'
 
 // const QuestionDetail = (props) => {
@@ -18,62 +18,87 @@ import * as ReactDOM from 'react-dom'
 //             {   likeCount > 0 ?
 //                 <span className = "badge bg-primary">{likeCount}</span>:''
 //             }
-            
 
 //         </div>
 //     </div>
 //     )
 // }
 
-
-
 class QuestionDetail extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      likeCount: 0,
+      dislikeCount: 0,
+    };
+    this.like = this.like.bind(this);
+    this.dislike = this.dislike.bind(this);
+  }
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            likeCount: 0 
-        }
-        this.like = this.like.bind(this)
-        this.dislike = this.dislike.bind(this)
-    }
+  like() {
+    this.setState(function (state) {
+      return {
+        likeCount: state.likeCount + 1,
+      };
+    });
+  }
+  dislike() {
+    this.setState(function (state) {
+      return {
+        dislikeCount: state.dislikeCount + 1,
+      };
+    });
+  }
 
-    like() {
-        this.setState(function(state) {
-            return {
-                likeCount: state.likeCount + 1
-            }
-        })
-    }
-    dislike() {
-        this.setState(function(state) {
-            return {
-                likeCount: state.likeCount - 1
-            }
-        })
-    }
+  render() {
+    return (
+      <div className="card rounded-0 mt-3">
+        <div className="card-body">
+          <h3 className="card-title">{this.props.question.title}</h3>
+          <p className="lead">
+            <span className="badge bg-primary">{this.props.question.tag}</span>
+          </p>
+          {/* <button className="btn btn-primary mt-1" onClick={this.like}>
+            Like
+          </button> */}
+          <button
+            type="button"
+            className="btn btn-primary position-relative"
+            onClick={this.like}
+            style={{ marginRight: '10mm' }}
+          >
+            Like
+            {this.state.likeCount > 0 ? (
+              <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                {this.state.likeCount}
+              </span>
+            ) : (
+              ''
+            )}
+          </button>
 
-    render(){
-        return(
-            <div className = "card rounded-0 mt-3" >
-            <div className = "card-body">
-                <h3 className = "card-title">{this.props.question.title}</h3>
-                <p className = "lead">
-                    <span className = "badge bg-primary">{this.props.question.tag}</span>
-                </p>
-                <button className="btn btn-primary mt-1" onClick={this.like}>Like</button>
-                <button className="btn btn-primary mt-1" onClick={this.dislike}>Dislike</button>
+          <button
+            type="button"
+            className="btn btn-primary position-relative"
+            onClick={this.dislike}
+          >
+            Dislike
+            {this.state.dislikeCount > 0 ? (
+              <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                {this.state.dislikeCount}
+              </span>
+            ) : (
+              ''
+            )}
+          </button>
+          {/* <button className="btn btn-primary mt-1" onClick={this.dislike}>Dislike</button>
                 {   this.state.likeCount > 0 ?
                     <span className = "badge bg-primary">{this.state.likeCount}</span>:''
-                }
-                
-
-            </div>
+                } */}
         </div>
-        )
-    }
+      </div>
+    );
+  }
 }
 
-
-
-export default QuestionDetail
+export default QuestionDetail;
