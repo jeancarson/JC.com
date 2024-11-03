fc45c64834008276c8597f7babc3f7259ade21df
jeancarson <jeanloucarson@gmail.com>
2024-11-03 12:42:32 +0000

    create API

--- app/javascript/react/src/components/QuestionList.jsx
+++ app/javascript/react/src/components/QuestionList.jsx
@@ -3,44 +3,7 @@ import * as ReactDOM from 'react-dom'
 import QuestionDetail from './QuestionDetail'
 
 const QuestionList = () => {
-    const questionsList = [
-        { 
-            id: 1,
-            title: "How to check if a id is present in a hash?", tag: "Ruby" 
-        },
-        { 
-            id: 2,
-            title: "What is the difference between strings and symbol?", tag: "Ruby" 
-        },        
-        { 
-            id: 3,
-            title: "What happens if you add two of the same ids in hash?", tag: "Ruby" 
-        },
-        { 
-            id: 4,
-            title: "How to delete a given id from a hash?", tag: "Ruby" 
-        },
-        { 
-            id: 5,
-            title: "How to check if two hashes are identical?", tag: "Ruby" 
-        },
-        { 
-            id: 6,
-            title: "How to combine two hashes in Ruby", tag: "Ruby" 
-        },
-        { 
-            id: 7,
-            title: "How to get uniwue ids from two hashes in Ruby?", tag: "Ruby" 
-        },
-        { 
-            id: 8,
-            title: "What does the has_id?, id?, include?, member? do?", tag: "Ruby" 
-        },
-        { 
-            id: 9,
-            title: "Does the order of keys matterwhen comparing two hashes?", tag: "Ruby" 
-        },
-    ]
+    
 
     return (
         <div className = "row"> 
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,34 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts 'Creating Questions...'
+questions = Question.create ([
+        { 
+            title: "How to check if a id is present in a hash?", tag: "Ruby" 
+        },
+        { 
+            title: "What is the difference between strings and symbol?", tag: "Ruby" 
+        },        
+        { 
+            title: "What happens if you add two of the same ids in hash?", tag: "Ruby" 
+        },
+        { 
+            title: "How to delete a given id from a hash?", tag: "Ruby" 
+        },
+        { 
+            title: "How to check if two hashes are identical?", tag: "Ruby" 
+        },
+        { 
+            title: "How to combine two hashes in Ruby", tag: "Ruby" 
+        },
+        { 
+            title: "How to get uniwue ids from two hashes in Ruby?", tag: "Ruby" 
+        },
+        { 
+            title: "What does the has_id?, id?, include?, member? do?", tag: "Ruby" 
+        },
+        { 
+            title: "Does the order of keys matterwhen comparing two hashes?", tag: "Ruby" 
+        }
+    ])
+puts 'Questions created!'
