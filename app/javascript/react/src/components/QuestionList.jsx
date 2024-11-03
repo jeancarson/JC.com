ba11bdee15f738fd10e48cfdc5ca6cb529ebc11f
jeancarson <jeanloucarson@gmail.com>
2024-11-03 13:12:32 +0000

    fetch API

--- app/javascript/react/src/components/QuestionList.jsx
+++ app/javascript/react/src/components/QuestionList.jsx
@@ -1,19 +1,33 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
-import QuestionDetail from './QuestionDetail'
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import QuestionDetail from './QuestionDetail';
+import { useState, useEffect } from 'react';
 
 const QuestionList = () => {
-    
+  const [questionsList, setQuestionsList] = useState([]);
+  const questionsUrl = 'http://localhost:3000/api/v1/questions';
 
-    return (
-        <div className = "row"> 
-            <div className = "col-lg-10 mx-auto">
-                {questionsList.map((question) =>
-                <QuestionDetail question = { question } key = { question.id}/>
-                )}
-            </div>
-        </div>
-    )
-}
+  const fetchQuestionList = () => {
+    fetch(questionsUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setQuestionsList(data);
+      });
+  };
+  useEffect(() => {
+    fetchQuestionList();
+  }, []);
+
+  return (
+    <div className="row">
+      <div className="col-lg-10 mx-auto">
+        {questionsList.map((question) => (
+          <QuestionDetail question={question} key={question.id} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default QuestionList;
