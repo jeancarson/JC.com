26d7e845493ef683f6e09954836a754840c5c3b1
jeancarson <jeanloucarson@gmail.com>
2024-12-26 22:29:57 +0000

    cv presentable

--- app/javascript/react/src/components/CV/CVOverview.jsx
+++ app/javascript/react/src/components/CV/CVOverview.jsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { animate, scroll } from 'framer-motion';
 import CVElement from './CVElement';
+import CVIntro from './CVIntro';
 import * as ReactDOM from 'react-dom/client';
 import '../../my-styles.css';
 
@@ -23,39 +24,126 @@ const CVOverview = () => {
     <div
       style={{ marginTop: '40px', marginBottom: '80px', textAlign: 'center' }}
     >
-      <h1 className="adale-mono-font" style={{ color: 'white' }}>
-        Here are all the things I've done. Pay attention
-      </h1>
+      <CVIntro />
       <CVElement
         title="Analog Devices"
-        description="Played a lot of pool and occasionally fought with some embedded C"
-        image="/images/analog-wires.jpg"
+        description={
+          <>
+            I completed a 14 week internship with this leading semiconductor
+            manufacturer. I worked in embedded C on encrypting data from a
+            sensor to a cloud ☁️ service for a new product. <br />
+            <br />
+            This was a research heavy role which required a lot of
+            experimentation as this had not been achieved with this hardware
+            previously <br />
+            <br />I also played a lot of pool 😅
+          </>
+        }
+        image="/images/outside-analog.jpg"
         ref={(el) => (sectionRefs.current[0] = el)} // Assign ref
       />
       <CVElement
         title="Intercom"
-        description="Did web developemnt using React and Ruby on Rails and ate a lot of chocolate"
+        description={
+          <>
+            I spent 10 weeks as an intern here, working in web development on
+            the Intercom Messenger team. <br />
+            <br />
+            I was using Ruby on Rails and React doing a mix of bug 🪳 fixes and
+            adding features to the messenger <br />
+            <br />
+            It was great to work in and really contribute to a production team
+            🎉
+          </>
+        }
         ref={(el) => (sectionRefs.current[1] = el)} // Assign ref
         image="/images/intercom-pool.jpg"
       />
       <CVElement
         title="ISE Block 4"
-        description="Strange exam structure and a LOT of subjects - AWS (where I hosted ClubHub), more software patters, C++ for event driven programming, functional programming (HAskall, yuck)"
+        description={
+          <>
+            In this block I studied software design patterns, data structures &
+            algorithms, functional programming (Haskell 😵‍💫), operating sytems,
+            computer architecture (basic electronics and building circuits ⚡)
+            and and hosted my ClubHub app from block 3 on AWS
+            <br />
+            <br />
+            We also learned about event driven programming through C++ and I
+            built a GUI for my version of the classic Zork game using QT <br />
+            <br />
+            This block was also heavy on innovation and professional
+            development, learning about things like podcasts & written
+            communication. We hosted a hackathon to explore software solutions
+            for local charities and later presented our ideas in the format of
+            the Amazon 6 page document
+            <br />
+          </>
+        }
+        image="/images/gui-zork.png"
         ref={(el) => (sectionRefs.current[2] = el)} // Assign ref
       />
       <CVElement
         title="ISE Block 3"
-        description="Hell on Earth. Machine learning and data science heavy, through python. I actually liked that part but just had NO TIME to do it bc we had to make a 'mini prject' Flask app called CLubHub used to make a sports club membership management system."
+        description={
+          <>
+            I worked with a group to build a web app in python using Flask and
+            an SQLite database. The app, 'ClubHub', was a sports club membership
+            management system. <br />
+            <br />
+            We studied maths for data science before moving on to learning about
+            data visualisation, data analytics and machine learning 🤖 <br />
+            <br />
+            Our final submission for the block was a machine learning pipeline.
+            We aimed to predict a woman's chance of surviving breast cancer
+            based on various factors. We found and cleaned a suitable database
+            before using methods such as Naive Bayes, random forest, and linear
+            and logistic regression. Unfortunately, our results were not
+            terribly accurate 🫣, but we learned a lot!
+          </>
+        }
+        image="/images/block3.png"
         ref={(el) => (sectionRefs.current[3] = el)} // Assign ref
       />
       <CVElement
         title="ISE Block 2"
-        description="Java taxi simulation app MUPPPP. Also there was an attempt to teach us docker and kubernetes but it was not successful. Dd a lot of software patterns which I really enjoed"
+        description={
+          <>
+            This block focussed on object oriented programming, with classes on
+            refactoring, code reviews and software design patterns <br />
+            <br />
+            We studied software testing, CICD, some Docker and Kubernetes, maths
+            🤓 and some more professional development <br />
+            <br />
+            As our final project we were tasked with building a taxi simulation
+            app in Java with a heavy focus on OOP principles. With my teammate I
+            made a terminal based app which presents a user alist of nearby
+            randomly moving 'ghost' taxi, which takes the optimal path to their
+            destination. Users can choose their ride based on price and rating,
+            and can rate the jouney at the end 🚕
+          </>
+        }
+        image="/images/block2-taxi.png"
         ref={(el) => (sectionRefs.current[4] = el)} // Assign ref
       />
       <CVElement
         title="ISE Block 1"
-        description="I did not know how to code when I came here LOL. I learned Java and made a quiz game, as well as studying things like discrete maths and oftware architechtures."
+        description={
+          <>
+            I entered this block with next to no coding experience, and began
+            with Java, as well as learning basic git and shell 🐚 programming
+            <br />
+            <br />
+            I studied computer organisation (some programming in assembly 🫨),
+            discrete mathematics and computer foundations <br />
+            <br />
+            At the end of the 8-week block we worked in pairs to build a quiz
+            app in java, which could be played in several game modes and
+            provided a user with an analysis of their performance both
+            personally and in comparison to others
+          </>
+        }
+        image="/images/block1.png"
         ref={(el) => (sectionRefs.current[5] = el)} // Assign ref
       />
       {/* {[...Array(10)].map((_, index) => (
