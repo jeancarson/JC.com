b4a4e40d69a37997207b71184ceb8e0da31f58d8
jeancarson <jeanloucarson@gmail.com>
2024-12-26 23:09:59 +0000

    database migrated

--- app/models/drink.rb
+++ app/models/drink.rb
@@ -1,8 +1,9 @@
 class Drink < ApplicationRecord
-    validates :name, presence: true, uniqueness: true
+    validates :name, presence: true, uniqueness: true, length: { maximum: 15 }
+    validates :description, presence: true, length: { maximum: 50 }
     validates :priceml, presence: true
-    validates :priceml, numericality: { greatergreater_than_or_equal_to_than: 0 }
-
+    validates :priceml, numericality: { greater_than_or_equal_to: 0 }
+    validates :imagepath, presence: true
 
     def self.ransackable_attributes(auth_object = nil)
         ["id", "id_value", "name", "description", "imagepath", "priceml", "color", "created_at", "updated_at"]
